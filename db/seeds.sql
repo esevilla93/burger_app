288b563d696a363d3b496fb64897193331d57861
Eddie Sevilla <eddiesevilla@Eddies-MacBook-Air.local>
2020-10-11 16:03:16 -0500

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+use burgers_db;
+
+INSERT INTO burgers 
+(burger_name, devoured)
+VALUES
+("Bulgogi Burger", false),
+("Dakgalbi Burger", false),
+("Daeji Burger", false)
+
+  SELECT * FROM burgers;
