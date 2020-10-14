695c59906d738825064fcbccae88930258d329ef
Eddie Sevilla <eddiesevilla@Eddies-MacBook-Air.local>
2020-10-14 11:36:06 -0500

    edited to deploy to heroku *please work*

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 var connection;
 
 if (process.envJAWSDB_URL) {
-    connection = mysql.createConnection(process.env.envJAWSDB_URL);
+    connection = mysql.createConnection(process.envJAWSDB_URL);
 } else {
 
     connection = mysql.createConnection({
